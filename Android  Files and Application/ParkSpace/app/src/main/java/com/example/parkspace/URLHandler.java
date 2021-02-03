911cae8d45ed848fadfa6deb1e9b6587a0bdb491
hetdesaii <39735064+hetdesaii@users.noreply.github.com>
2021-02-04 02:04:27 +0530

    URL String change
    Added slash at the end of the url

--- Android  Files and Application/ParkSpace/app/src/main/java/com/example/parkspace/URLHandler.java
+++ Android  Files and Application/ParkSpace/app/src/main/java/com/example/parkspace/URLHandler.java
@@ -2,7 +2,7 @@ package com.example.parkspace;
 
 public class URLHandler {
 
-    protected static final String requestedURL = "https://skmyjjzg.p71.rt3.io/";
+    protected static final String requestedURL = "https://skmyjjzg.p71.rt3.io" + "/";
 
     private URLHandler() {}
 }
